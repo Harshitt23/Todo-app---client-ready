cbffd4137825a3aa81bea86ebf7ea4ebece5a67b
Coding-master <harshitr2308@gmail.com>
2025-11-29 19:53:01 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -5,25 +5,15 @@ app.use(express.json()); // middleware to read JSON body
 
 // POST route to create a todo
 app.post("/todo", function (req, res) {
-    console.log(req.body);     // logs the data sent by frontend
-
-    // normally you would store it in DB, for now we just send back a response
-    res.json({
-        message: "Todo created successfully",
-        data: req.body
-    });
+   
 });
 
 app.get("/todos", function (req, res) {
-    res.json({
-        message: "List of todos"
-    });
+  
 });
 
 app.put("/completed", function (req, res) {
-    res.json({
-        message: "Todo marked as completed"
-    });
+  
 });
 
 app.listen(3000, function () {
